59e409aa60d2a1c57174e09fd89b4ff0c89792c1
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-02-20 10:48:59 +0200

    destructing practiced

---
+++ destructing.js
@@ -0,0 +1,5 @@
+const { age, name, profession } = { name: 'mizan', age: 30, profession: 'worker', salary: 'not bad' };
+console.log(age);
+
+const [first, last, third] = [12, 34, 56, 67, 78];
+console.log(third);
